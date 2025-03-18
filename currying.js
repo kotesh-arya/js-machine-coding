8ee9aaac85c5a8df2887ffba7ca51aa6b041057a
kotesh-arya <koteswarraomudila@gmail.com>
2025-03-18 23:17:26 +0530

    TASK-3: Infinite currying using recursion

--- currying.js
+++ currying.js
@@ -1,14 +1,32 @@
 
 // Sum operation using Currying in js
 
-function currySum(first) {
-    return function (second) {
-        return function (third) {
-            console.log(first + second + third)
-        }
-    }
+// function currySum(first) {
+//     return function (second) {
+//         return function (third) {
+//             console.log(first + second + third)
+//         }
+//     }
+// }
 
+
+// currySum(4)(3)(2); // 10
+
+
+// Infinite currying
+
+function curriedSum(a) {
+    return function (b) {
+
+        if (b !== undefined) {
+            return curriedSum(a + b);   
+        } else {
+            return a
+        }
+    };
 }
 
 
-currySum(4)(3)(2); // 10
+console.log(curriedSum(2)(3)()); // 2 + 3 = 5
+
+console.log(curriedSum(5)(4)(3)(2)(1)()); // 5 + 4 + 3 + 2 + 1 = 15
