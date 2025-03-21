6c8c9f11d722f205562dddf6cc36ecf85a77b5cc
kotesh-arya <koteswarraomudila@gmail.com>
2025-03-21 23:20:22 +0530

    TOPIC-2: Random floating-point number between limits

--- interview-topics.js
+++ interview-topics.js
@@ -48,8 +48,8 @@ test();
 
 // -------------------------------
 
-console.log(foo1()); // I am a declaration!
-console.log(foo2()); // TypeError: foo2 is not a function
+// console.log(foo1()); // I am a declaration!
+// console.log(foo2()); // TypeError: foo2 is not a function
 
 function foo1() {
     return "I am a declaration!";
@@ -62,7 +62,7 @@ var foo2 = function () {
 
 // -------------💣 HOISTING IN LOOPS  💣----------------------
 for (var i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i), 1000);
+    // setTimeout(() => console.log(i), 1000);
 }
 //   💡 Think: Why does it log 3 3 3 instead of 0 1 2 ?
 
@@ -71,3 +71,37 @@ for (var i = 0; i < 3; i++) {
 // Step-2 : Coming to the setTimeout, even before the line of code that logs the i value, gets executed. The loop will not wait as loops are synchronous in js, the value of I will be 3 before the 1000ms time elapsed.
 
 // Hence the value of i logged three times will be 3 3 3
+
+
+
+// 21 March 2025 [Friday]
+
+// console.log(0.1 + 0.2);
+
+
+// Write the function random(min, max) to generate a random floating-point number from min to max (not including max).
+
+// Examples of its work:
+
+// alert( random(1, 5) ); // 1.2345623452
+// alert( random(1, 5) ); // 3.7894332423
+// alert( random(1, 5) ); // 4.3435234525
+
+
+
+function random(start, end) {
+    console.log(start, end);
+    // 💎If we multiply a random number from using ramdom method from 0 to 1
+    //  then on multiplying that number with a integer will give a RANDOM NUMBER LESSTHAN the multiplied integer.
+
+    console.log(start + Math.random() * (end - start));
+}
+// random(1,4);
+
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log(randomInteger(1, 4), "comment result");
+let startRandomNum = Math.random() * 5;
