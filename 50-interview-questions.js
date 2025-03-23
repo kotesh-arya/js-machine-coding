9d50323cd746a7ec79a3ee02ff1b35c4fb8fe3c1
kotesh-arya <koteswarraomudila@gmail.com>
2025-03-23 15:47:54 +0530

    Question 2 : Hash Tag Generator

--- 50-interview-questions.js
+++ 50-interview-questions.js
@@ -45,12 +45,70 @@ function findLongestWord(sentence) {
     // console.log(longestWordusingReduce, "using reduce");
 }
 
-console.log(findLongestWord(
-    "Watch Thapa Technical javascript awesomethapatechnical course on youtube"
-));
-console.log(findLongestWord(" "));
+// console.log(findLongestWord(
+//     "Watch Thapa Technical javascript awesomethapatechnical course on youtube"
+// ));
+// console.log(findLongestWord(" "));
 
-console.log(findLongestWord("Hello!! World123"));
+// console.log(findLongestWord("Hello!! World123"));
 
 
 // console.log(" ".trim().length, "empty string after trimmming");
+
+//*-------------------------------------------
+//*  Question 2 : Hash Tag Generator
+//*-------------------------------------------
+
+//? You are required to implement a function generateHash that generates a hash tag from a given input string. The hash tag should be constructed as follows:
+
+//? The input string should be converted to a hash tag format, where each word is capitalized and concatenated together without spaces.
+//? If the length of the input string is greater than 280 characters or if the input string is empty or contains only whitespace, the function should return false.
+//? Otherwise, the function should return the generated hash tag prefixed with #.
+
+//* Write a function generateHash to accomplish this task.
+
+
+// Input String  |	Expected Output
+// "hello world" |	"#HelloWorld"
+// " coding is fun " |	"#CodingIsFun"
+// "this is a very long sentence that should be converted to a hashtag" | "#ThisIsAVeryLongSentenceThatShouldBeConvertedToAHashtag"
+
+// "" (empty string) |	false
+// " " (only whitespace) |	false
+// "a".repeat(281) (string longer than 280) |	false
+// "single" |	"#Single"
+
+
+function generateHash(text) {
+    // console.log(text);
+    let trimmedText = text.trim();
+    if (trimmedText.length === 0) {
+        return false;
+    }
+    if (trimmedText.length > 280) {
+        return false;
+    }
+    let splittedText = trimmedText.split(" ");
+    // console.log(splittedText);
+    let result = "";
+    for (let i = 0; i < splittedText.length; i++) {
+        let charSplitArr = splittedText[i].split("");
+        // console.log( result += charSplitArr[0].toUpperCase() + charSplitArr.splice(1).join(""));
+        result += (charSplitArr[0].toUpperCase() + charSplitArr.splice(1).join(""));
+        // console.log(result, "hashed result inside loop");
+    }
+    return "#" + result;
+}
+
+console.log(generateHash("hello world"));
+
+console.log(generateHash(" coding is fun "));
+
+console.log(generateHash("this is a very long sentence that should be converted to a hashtag"));
+
+
+console.log(generateHash(""));
+console.log(generateHash(" "));
+console.log(generateHash("a".repeat(281)));
+console.log(generateHash("single"));
+
