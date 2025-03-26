85ee47b345b2529cd67375c74a85f3449ddf6d1d
kotesh-arya <koteswarraomudila@gmail.com>
2025-03-26 21:18:31 +0530

    Question 19: Recursive Number Range Generator

--- 50-interview-questions.js
+++ 50-interview-questions.js
@@ -647,3 +647,36 @@ console.log(simplePasswordValidator("afkdsfadsf")); // Output: false
 console.log(simplePasswordValidator("afkdsfadsf1")); // Output: false
 console.log(simplePasswordValidator("afkdsfadsf1A")); // Output: true
 console.log(simplePasswordValidator("afkdsfadsf1Aa")); // Output: true
+
+
+
+//*-----------------------------------------------
+//* Q19.Recursive Number Range Generator
+//*-----------------------------------------------
+
+//? Write a recursive function called numberRangeRecursive that generates an array containing consecutive numbers from a to b (inclusive).
+
+//* Input:
+//? a: An integer representing the starting number of the range.
+//? b: An integer representing the ending number of the range.
+//? arr: An optional parameter representing the array to store the numbers in the range. It defaults to an empty array.
+
+//* Output:
+//? An array containing consecutive numbers from a to b (inclusive).
+
+//* Constraints:
+//? a and b will be integers.
+//? a will be less than or equal to b.
+
+
+function numberRangeRecursive(a, b, rangeArr = []) {
+    if (a <= b) {
+        rangeArr.push(a);
+        return numberRangeRecursive(a + 1, b, rangeArr) // passing the rangeArr to for a persistent array throughout the recursive function invocation.
+    }
+    return rangeArr;
+}
+
+console.log(numberRangeRecursive(0, 5)); // Output: [0, 1, 2, 3, 4, 5]
+console.log(numberRangeRecursive(3, 7)); // Output: [3, 4, 5, 6, 7]
+console.log(numberRangeRecursive(-2, 2)); // Output: [-2, -1, 0, 1, 2]
