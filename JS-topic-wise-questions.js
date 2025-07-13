28cbd3482ead3598667f03485e1a34ea75ea02e2
kotesh-arya <koteswarraomudila@gmail.com>
2025-07-13 12:22:03 +0530

    Call, Apply & Bind and polyfills

--- JS-topic-wise-questions.js
+++ JS-topic-wise-questions.js
@@ -53,29 +53,101 @@
 
 
 // // Factory functions(functions that return objects on invocation) an alternative of classes in creating objects
-// // function createCounter() {
-// //     let count = 0;
-// //     return {
-// //         increment() { count++; return this; },
-// //         get() { return count; }
-// //     };
-// // }
-// // let counter1 = createCounter();
-// // console.log(counter1);
-// // console.log(counter1.get(), "value if the property in the created object instance BEFORE incermenting");
-// // console.log(counter1.increment());
-// // console.log(counter1.get(), "value if the property in the created object instance AFTER incermenting");
+// function createCounter() {
+//     let count = 0;
+//     return {
+//         increment() { count++; return this; },
+//         get() { return count; }
+//     };
+// }
+// let counter1 = createCounter();
+// console.log(counter1);
+// console.log(counter1.get(), "value if the property in the created object instance BEFORE incermenting");
+// console.log(counter1.increment());
+// console.log(counter1.get(), "value if the property in the created object instance AFTER incermenting");
 
 // // ---------------------------------------------------------------------
 
 // Prototypes, Prototypal Inheritance
-// const animal = {
-//     eats: true
-// }
-// const rabbit = {
-//     jumps: true
-// }
+const animal = {
+    eats: true
+}
+const rabbit = {
+    jumps: true
+}
 
-// rabbit.__proto__ = animal;
+rabbit.__proto__ = animal;
 
-// console.log(rabbit.eats); // true - inherited property from the prototype object
+console.log(rabbit.eats); // true - inherited property from the prototype object
+
+
+function greet(greeting) {
+    return `${greeting} ${this.name} `;
+}
+
+let user = {
+    name: "arya"
+}
+
+
+// call polyfill
+
+Function.prototype.myCall = function (context, ...params) {
+    if (typeof this !== "function") {
+        throw new TypeError(this + " is not callable");
+    }
+
+    context = context || globalThis;
+
+    const fnSymbol = Symbol();
+    context[fnSymbol] = this;
+
+    let result = context[fnSymbol](...params);
+
+    delete context[fnSymbol];
+    return result;
+
+};
+console.log(greet.myCall(user, "Hi"));  // Hi arya
+
+
+// apply polyfill
+
+Function.prototype.myApply = function (context, paramsArr) {
+    if (typeof this !== "function") {
+        throw new TypeError(this + " is not callable");
+    }
+    if (paramsArr !== undefined && !Array.isArray(paramsArr)) throw new TypeError("CreateListFromArrayLike called on non-object");
+
+    context = context || globalThis;
+
+    let fnSymbol = Symbol();
+    console.log("symbol returned", fnSymbol);
+
+    context[fnSymbol] = this;
+
+    let result = context[fnSymbol](...paramsArr);
+
+    delete context[fnSymbol];
+
+    return result;
+
+};
+console.log(greet.myApply(user, ["Hello"]));  // Hello arya 
+
+
+// bind polyfill
+
+Function.prototype.myBind = function (context, ...presetArgs) {
+    let functionToBeBounded = this;
+    if (typeof functionToBeBounded !== "function") throw new TypeError("Bind must be called on a function");
+
+    return function boundedFunction(...laterArgs) {
+        return functionToBeBounded.call(this instanceof boundedFunction ? this : context, ...presetArgs, ...laterArgs);
+    };
+
+};
+
+
+const bound = greet.myBind(user);
+console.log(bound("Yo..!")); // Yo..! arya 
